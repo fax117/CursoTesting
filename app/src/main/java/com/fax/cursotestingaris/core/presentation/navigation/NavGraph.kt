ee93429546f8087ad0a082c234582b9aa5b9ccbd
Fabricio Andre Fuentes Fuentes <38862497+fax117@users.noreply.github.com>
2026-05-05 15:29:24 -0600

    Product Detail Finished
    Product detail is being shown correctly

--- app/src/main/java/com/fax/cursotestingaris/core/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/fax/cursotestingaris/core/presentation/navigation/NavGraph.kt
@@ -8,6 +8,7 @@ import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
+import com.fax.cursotestingaris.product_detail.presentation.ProductDetailScreen
 import com.fax.cursotestingaris.product_list.presentation.ProductListScreen
 import com.fax.cursotestingaris.settings.presentation.SettingsScreen
 
@@ -16,7 +17,16 @@ fun NavGraph() {
     val backStack = rememberNavBackStack(Screen.ProductList)
     val entries = entryProvider<NavKey> {
         entry<Screen.ProductList> {
-            ProductListScreen(navigateToSettings = {backStack.add(Screen.Settings)})
+            ProductListScreen(
+                navigateToSettings = { backStack.add(Screen.Settings) },
+                navigateToProductDetail = { productId ->
+                    backStack.add(
+                        Screen.ProductDetail(
+                            productId
+                        )
+                    )
+                }
+            )
         }
         entry<Screen.Cart> {
             Text(text = "Cart", fontSize = 30.sp)
@@ -24,8 +34,10 @@ fun NavGraph() {
         entry<Screen.Settings> {
             SettingsScreen(onBack = { backStack.removeLastOrNull() })
         }
-        entry<Screen.ProductDetail> {
-            Text(text = "Product Detail", fontSize = 30.sp)
+        entry<Screen.ProductDetail> { route ->
+            ProductDetailScreen(
+                productId = route.productId,
+                onBack = { backStack.removeLastOrNull() })
         }
     }
 
